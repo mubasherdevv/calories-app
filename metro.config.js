a41222094ad91ec9e5c95f7e7fb6f68730c4bf7c
mubasherdevv <seoworking009@gmail.com>
2026-06-02 21:02:59 +0500

    fix: ignore android/ios build folders in Metro blockList to prevent watch mode crash on Windows

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,15 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro')
 
 const config = getSentryExpoConfig(__dirname)
 
+if (!config.resolver) {
+  config.resolver = {}
+}
+
+config.resolver.blockList = [
+  /.*[\\/]android[\\/]build[\\/].*/,
+  /.*[\\/]android[\\/]app[\\/]build[\\/].*/,
+  /.*[\\/]ios[\\/]build[\\/].*/,
+  /.*[\\/]\.git[\\/].*/
+]
+
 module.exports = config
